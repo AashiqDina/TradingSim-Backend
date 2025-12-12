4bc87b5525fcccefd400b2b481c75606568c24a9
Aashiq Dina <98742552+AashiqDina@users.noreply.github.com>
2025-12-12 14:54:54 +0000

    Refactor StockService to use environment variable for API key
    Updated StockService to use environment variable for API key.

--- Services/StockService.cs
+++ Services/StockService.cs
@@ -15,7 +15,7 @@ namespace TradingSimulator_Backend.Services
     {
         private readonly HttpClient _httpClient;
         private readonly AppDbContext _context;
-        private readonly string _apiKey = "";
+        private readonly string _apiKey = Environment.GetEnvironmentVariable("STOCK_API_KEY");
         
         private static ConcurrentDictionary<string, (decimal? Price, DateTime Timestamp)> _stockCache = new ConcurrentDictionary<string, (decimal? Price, DateTime Timestamp)>();
         private static ConcurrentDictionary<string, (string? Logo, string? Name)> _stockImageCache = new ConcurrentDictionary<string, (string? Logo, string? Name)>();
@@ -639,4 +639,5 @@ namespace TradingSimulator_Backend.Services
         }
     }
 }
+
 
