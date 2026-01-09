fd2b606a2fb1f46140534d82d587a05ca8942a45
Aashiq Dina <98742552+AashiqDina@users.noreply.github.com>
2026-01-09 02:48:49 +0000

    Add data annotations to Portfolio class
    Added data annotations for database mapping.

--- Models/Portfolio.cs
+++ Models/Portfolio.cs
@@ -1,19 +1,22 @@
-using TradingSimulator_Backend.Models;
 
 public class Portfolio{
+    [Column("id")]
     public long Id { get; set; }
 
+    [Column("user_id")]
     public long UserId { get; set; }
+
+    [ForeignKey("UserId")]
     public User User { get; set; } = null!;
 
     public List<Stock> Stocks { get; set; } = new();
 
-    public decimal TotalInvested =>
-        Stocks.Sum(stock => stock.PurchasePrice * stock.Quantity);
+    [NotMapped]
+    public decimal TotalInvested => Stocks.Sum(stock => stock.PurchasePrice * stock.Quantity);
 
-    public decimal CurrentValue =>
-        Stocks.Sum(stock => stock.TotalValue);
+    [NotMapped]
+    public decimal CurrentValue => Stocks.Sum(stock => stock.TotalValue);
 
-    public decimal ProfitLoss =>
-        CurrentValue - TotalInvested;
+    [NotMapped]
+    public decimal ProfitLoss => CurrentValue - TotalInvested;
 }
