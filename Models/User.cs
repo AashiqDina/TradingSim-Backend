c4e2ee8564dd83cdb552057f8b258baafdac9683
Aashiq Dina <98742552+AashiqDina@users.noreply.github.com>
2026-01-09 02:57:50 +0000

    Refactor User class structure and formatting

--- Models/User.cs
+++ Models/User.cs
@@ -1,27 +1,26 @@
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace TradingSimulator_Backend.Models{
-    [Table("users")]
-    public class User{
-        [Column("id")]
-        public long Id { get; set; } 
 
-        [Column("username")]
-        public required string Username { get; set; }
+public class User{
+    [Column("id")]
+    public long Id { get; set; } 
 
-        [Column("password")]
-        public required string Password { get; set; }
+    [Column("username")]
+    public required string Username { get; set; }
 
-        [Column("investedamount")]
-        public float InvestedAmount { get; set; } 
+    [Column("password")]
+    public required string Password { get; set; }
 
-        [Column("currentvalue")]
-        public float CurrentValue { get; set; }
+    [Column("investedamount")]
+    public float InvestedAmount { get; set; } 
 
-        [Column("profitloss")]
-        public float ProfitLoss { get; set; }
+    [Column("currentvalue")]
+    public float CurrentValue { get; set; }
 
-        public Friends? Friends { get; set; }
-    }
+    [Column("profitloss")]
+    public float ProfitLoss { get; set; }
+
+    public Friends? Friends { get; set; }
 }
+
 
