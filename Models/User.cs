8c2d757dd086a563a235677ca107e3187b29b5e7
Aashiq Dina <98742552+AashiqDina@users.noreply.github.com>
2026-01-09 02:08:25 +0000

    Refactor User model with data annotations
    Added data annotations for database mapping in User class.

--- Models/User.cs
+++ Models/User.cs
@@ -1,12 +1,24 @@
-namespace TradingSimulator_Backend.Models
-{
-    public class User
-    {
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TradingSimulator_Backend.Models{
+    [Table("users")]
+    public class User{
+        [Column("id")]
         public int Id { get; set; }
+
+        [Column("username")]
         public required string Username { get; set; }
+
+        [Column("password")]
         public required string Password { get; set; }
-        public decimal InvestedAmount { get; set;}
+
+        [Column("investedamount")]
+        public decimal InvestedAmount { get; set; }
+
+        [Column("currentvalue")]
         public decimal CurrentValue { get; set; }
+
+        [Column("profitloss")]
         public decimal ProfitLoss { get; set; }
 
         public Friends? Friends { get; set; }
