3a906e9a3c8fa11dd5f859d235bb5bc2c132ff18
Aashiq Dina <98742552+AashiqDina@users.noreply.github.com>
2026-01-09 02:20:30 +0000

    Change Id type to long and ProfitLoss to float

--- Models/UserObj.cs
+++ Models/UserObj.cs
@@ -1,6 +1,6 @@
 public class UserObj
 {
-    public int Id { get; set; }
+    public long Id { get; set; } 
     public string Username { get; set; } = string.Empty;
-    public decimal ProfitLoss { get; set; }
+    public float ProfitLoss { get; set; } 
 }
