171d55468516e820c5f3aed23640fd6841d94b72
Aashiq Dina <98742552+AashiqDina@users.noreply.github.com>
2026-01-10 14:50:59 +0000

    Refactor Friends class to use User type in lists

--- Models/Friends.cs
+++ Models/Friends.cs
@@ -1,14 +1,12 @@
 using System.ComponentModel.DataAnnotations.Schema;
 
-    public class Friends{
-        [Column("userid")]
-        public long UserId { get; set; }
+public class Friends
+{
+    public long UserId { get; set; }
+    public User User { get; set; } = null!;
 
-        public User User { get; set; } = null!;
-
-        public List<UserObj> FriendsList { get; set; } = new();
-        public List<UserObj> SentRequests { get; set; } = new();
-        public List<UserObj> ReceivedRequests { get; set; } = new();
-    }
-
+    public List<User> FriendsList { get; set; } = new();
+    public List<User> SentRequests { get; set; } = new();
+    public List<User> ReceivedRequests { get; set; } = new();
+}
 
