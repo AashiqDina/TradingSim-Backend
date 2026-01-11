e0b94b51815b0d7d7a58cd747fb4c1905855dd08
Aashiq Dina <98742552+AashiqDina@users.noreply.github.com>
2026-01-11 12:34:37 +0000

    Update IStockService.cs

--- Services/IStockService.cs
+++ Services/IStockService.cs
@@ -6,6 +6,7 @@ using TradingSimulator_Backend.Models;
 
 public interface IStockService
 {
+    Task<ApiResponse<bool>> DeleteStockLogo(string symbol);
     Task<ApiResponse<decimal?>?> GetStockPriceAsync(string symbol);
     Task<Dictionary<string, ApiResponse<decimal?>?>> GetMultipleStockPricesAsync(List<string> symbols);
     Task<ApiResponse<string?>?> GetStockImage(string symbol);
@@ -20,3 +21,4 @@ public interface IStockService
     string[] getTrendingList();
     void updateTrendingMap(string symbol);
 }
+
