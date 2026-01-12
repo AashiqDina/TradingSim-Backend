6048a7e3abb070924c9ace50c665fc118d8e11e9
Aashiq Dina <98742552+AashiqDina@users.noreply.github.com>
2026-01-12 23:59:10 +0000

    Update AppDbContext.cs

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -22,49 +22,95 @@ namespace TradingSimulator_Backend.Data
             modelBuilder.Entity<Stock>().ToTable("stocks");
             modelBuilder.Entity<StockLogoName>().ToTable("stocklogoname");
             modelBuilder.Entity<StockHistory>().ToTable("stockhistory");
-
-            modelBuilder.Entity<User>()
-                .OwnsOne(u => u.Friends, f =>
-                {
-                    f.OwnsMany(x => x.FriendsList);
-                    f.OwnsMany(x => x.SentRequests);
-                    f.OwnsMany(x => x.ReceivedRequests);
-                });
-
-            // modelBuilder.Entity<User>()
-            //     .HasOne(u => u.Friends)
-            //     .WithOne(f => f.User)
-            //     .HasForeignKey<Friends>(f => f.UserId);
-
-            modelBuilder.Entity<Friends>()
-                .HasOne(f => f.User)
-                .WithOne(u => u.Friends)
-                .HasForeignKey<Friends>(f => f.UserId);
-
+        
+            // Friends setup
+            modelBuilder.Entity<Friends>(f =>
+            {
+                f.HasOne(f => f.User)
+                 .WithOne()
+                 .HasForeignKey<Friends>(f => f.UserId)
+                 .OnDelete(DeleteBehavior.Cascade);
+        
+                f.OwnsMany(f => f.SentRequests);
+                f.OwnsMany(f => f.ReceivedRequests);
+                f.OwnsMany(f => f.FriendsList);
+            });
+        
             modelBuilder.Entity<Portfolio>()
                 .HasOne(p => p.User)
                 .WithOne()
                 .HasForeignKey<Portfolio>(p => p.UserId)
                 .OnDelete(DeleteBehavior.Cascade);
-
+        
             modelBuilder.Entity<StockLogoName>()
                 .HasKey(s => s.Symbol);
-
+        
             modelBuilder.Entity<StockHistory>()
                 .HasOne(h => h.Stock)
                 .WithMany(s => s.History)
                 .HasForeignKey(h => h.StockId)
                 .OnDelete(DeleteBehavior.Cascade);
-
+        
             modelBuilder.Entity<Stock>()
                 .HasOne(s => s.Portfolio)
                 .WithMany(p => p.Stocks)
                 .HasForeignKey(s => s.PortfolioId)
                 .OnDelete(DeleteBehavior.Cascade);
-            
         }
+
+
+        // protected override void OnModelCreating(ModelBuilder modelBuilder)
+        // {
+        //     modelBuilder.Entity<User>().ToTable("users");
+        //     modelBuilder.Entity<Friends>().ToTable("friends");
+        //     modelBuilder.Entity<Portfolio>().ToTable("portfolios");
+        //     modelBuilder.Entity<Stock>().ToTable("stocks");
+        //     modelBuilder.Entity<StockLogoName>().ToTable("stocklogoname");
+        //     modelBuilder.Entity<StockHistory>().ToTable("stockhistory");
+
+        //     modelBuilder.Entity<User>()
+        //         .OwnsOne(u => u.Friends, f =>
+        //         {
+        //             f.OwnsMany(x => x.FriendsList);
+        //             f.OwnsMany(x => x.SentRequests);
+        //             f.OwnsMany(x => x.ReceivedRequests);
+        //         });
+
+        //     // modelBuilder.Entity<User>()
+        //     //     .HasOne(u => u.Friends)
+        //     //     .WithOne(f => f.User)
+        //     //     .HasForeignKey<Friends>(f => f.UserId);
+
+        //     modelBuilder.Entity<Friends>()
+        //         .HasOne(f => f.User)
+        //         .WithOne(u => u.Friends)
+        //         .HasForeignKey<Friends>(f => f.UserId);
+
+        //     modelBuilder.Entity<Portfolio>()
+        //         .HasOne(p => p.User)
+        //         .WithOne()
+        //         .HasForeignKey<Portfolio>(p => p.UserId)
+        //         .OnDelete(DeleteBehavior.Cascade);
+
+        //     modelBuilder.Entity<StockLogoName>()
+        //         .HasKey(s => s.Symbol);
+
+        //     modelBuilder.Entity<StockHistory>()
+        //         .HasOne(h => h.Stock)
+        //         .WithMany(s => s.History)
+        //         .HasForeignKey(h => h.StockId)
+        //         .OnDelete(DeleteBehavior.Cascade);
+
+        //     modelBuilder.Entity<Stock>()
+        //         .HasOne(s => s.Portfolio)
+        //         .WithMany(p => p.Stocks)
+        //         .HasForeignKey(s => s.PortfolioId)
+        //         .OnDelete(DeleteBehavior.Cascade);
+            
+        // }
     }
 }
+
 
 
 
