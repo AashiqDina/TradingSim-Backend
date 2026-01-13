7c844035478334c2cf755bb2795c44f3b88f3362
Aashiq Dina <98742552+AashiqDina@users.noreply.github.com>
2026-01-13 01:04:07 +0000

    Update User.cs

--- Models/User.cs
+++ Models/User.cs
@@ -1,7 +1,8 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 
-
-public class User{
+public class User
+{
     [Column("id")]
     public long Id { get; set; } 
 
@@ -20,7 +21,41 @@ public class User{
     [Column("profitloss")]
     public float ProfitLoss { get; set; }
 
-    public Friends? Friends { get; set; }
+    // Navigation properties to relational tables
+    public ICollection<UserFriend> FriendsList { get; set; } = new List<UserFriend>();
+    public ICollection<UserSentRequest> SentRequests { get; set; } = new List<UserSentRequest>();
+    public ICollection<UserReceivedRequest> ReceivedRequests { get; set; } = new List<UserReceivedRequest>();
 }
 
+// Friend entity
+public class UserFriend
+{
+    public long Id { get; set; } // Identity column
+    public long FriendsUserId { get; set; } // FK to User
+    public User FriendsUser { get; set; }   // Navigation back to User
 
+    public string Username { get; set; }
+    public float ProfitLoss { get; set; }
+}
+
+// Sent request entity
+public class UserSentRequest
+{
+    public long Id { get; set; } // Identity column
+    public long FriendsUserId { get; set; } // FK to User
+    public User FriendsUser { get; set; }
+
+    public string Username { get; set; }
+    public float ProfitLoss { get; set; }
+}
+
+// Received request entity
+public class UserReceivedRequest
+{
+    public long Id { get; set; } // Identity column
+    public long FriendsUserId { get; set; } // FK to User
+    public User FriendsUser { get; set; }
+
+    public string Username { get; set; }
+    public float ProfitLoss { get; set; }
+}
