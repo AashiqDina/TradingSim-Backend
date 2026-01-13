83753fd69f568dfdd83ae0110310369a1dc81937
Aashiq Dina <98742552+AashiqDina@users.noreply.github.com>
2026-01-13 02:13:43 +0000

    Update LoginRequest.cs

--- Models/LoginRequest.cs
+++ Models/LoginRequest.cs
@@ -1,4 +1,4 @@
-namespace TradingSim-Backend.Models
+namespace TradingSimulator_Backend.Models
 {
     public class LoginRequest
     {
@@ -7,3 +7,4 @@ namespace TradingSim-Backend.Models
     }
 
 }
+
