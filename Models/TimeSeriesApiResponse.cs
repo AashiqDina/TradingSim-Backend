1392495eb45619a49d9913a777ca834c47a3d8ab
Aashiq Dina <98742552+AashiqDina@users.noreply.github.com>
2026-01-13 02:16:41 +0000

    Update TimeSeriesApiResponse.cs

--- Models/TimeSeriesApiResponse.cs
+++ Models/TimeSeriesApiResponse.cs
@@ -1,5 +1,5 @@
 using Newtonsoft.Json;
-using TradingSim-Backend.Models;
+using TradingSimulator_Backend.Models
 
 public class TimeSeriesApiResponse
 {
@@ -13,3 +13,4 @@ public class TimeSeriesApiResponse
     public List<StockFullHistoryPoint>? Values { get; set; }
 
 }
+
