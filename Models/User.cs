7d3b5f915374fc28471d9f8eebca571360abd718
Aashiq Dina <98742552+AashiqDina@users.noreply.github.com>
2026-01-13 01:40:40 +0000

    Update User.cs

--- Models/User.cs
+++ Models/User.cs
@@ -1,61 +1,82 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 
-public class User
+namespace TradingSimulator_Backend.Models
 {
-    [Column("id")]
-    public long Id { get; set; } 
+    public class User
+    {
+        [Column("id")]
+        public long Id { get; set; } 
 
-    [Column("username")]
-    public required string Username { get; set; }
+        [Column("username")]
+        public required string Username { get; set; }
 
-    [Column("password")]
-    public required string Password { get; set; }
+        [Column("password")]
+        public required string Password { get; set; }
 
-    [Column("investedamount")]
-    public float InvestedAmount { get; set; } 
+        [Column("investedamount")]
+        public float InvestedAmount { get; set; } 
 
-    [Column("currentvalue")]
-    public float CurrentValue { get; set; }
+        [Column("currentvalue")]
+        public float CurrentValue { get; set; }
 
-    [Column("profitloss")]
-    public float ProfitLoss { get; set; }
+        [Column("profitloss")]
+        public float ProfitLoss { get; set; }
 
-    // Navigation properties to relational tables
-    public ICollection<UserFriend> FriendsList { get; set; } = new List<UserFriend>();
-    public ICollection<UserSentRequest> SentRequests { get; set; } = new List<UserSentRequest>();
-    public ICollection<UserReceivedRequest> ReceivedRequests { get; set; } = new List<UserReceivedRequest>();
-}
+        // --- Navigation properties ---
 
-// Friend entity
-public class UserFriend
-{
-    public long Id { get; set; } // Identity column
-    public long FriendsUserId { get; set; } // FK to User
-    public User FriendsUser { get; set; }   // Navigation back to User
+        // Portfolio (1:1)
+        public Portfolio? Portfolio { get; set; }
 
-    public string Username { get; set; }
-    public float ProfitLoss { get; set; }
-}
+        // Friends (1:M)
+        public ICollection<UserFriend> FriendsList { get; set; } = new List<UserFriend>();
 
-// Sent request entity
-public class UserSentRequest
-{
-    public long Id { get; set; } // Identity column
-    public long FriendsUserId { get; set; } // FK to User
-    public User FriendsUser { get; set; }
+        // Sent requests
+        public ICollection<UserSentRequest> SentRequests { get; set; } = new List<UserSentRequest>();
 
-    public string Username { get; set; }
-    public float ProfitLoss { get; set; }
-}
+        // Received requests
+        public ICollection<UserReceivedRequest> ReceivedRequests { get; set; } = new List<UserReceivedRequest>();
+    }
 
-// Received request entity
-public class UserReceivedRequest
-{
-    public long Id { get; set; } // Identity column
-    public long FriendsUserId { get; set; } // FK to User
-    public User FriendsUser { get; set; }
+    public class UserFriend
+    {
+        public long Id { get; set; }
 
-    public string Username { get; set; }
-    public float ProfitLoss { get; set; }
+        // Foreign key to parent User
+        public long UserId { get; set; }
+        public User User { get; set; } = null!;
+
+        // Friend info
+        public long FriendId { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public float ProfitLoss { get; set; }
+    }
+
+    public class UserSentRequest
+    {
+        public long Id { get; set; }
+
+        // Foreign key to parent User
+        public long UserId { get; set; }
+        public User User { get; set; } = null!;
+
+        // Target user info
+        public long TargetUserId { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public float ProfitLoss { get; set; }
+    }
+
+    public class UserReceivedRequest
+    {
+        public long Id { get; set; }
+
+        // Foreign key to parent User
+        public long UserId { get; set; }
+        public User User { get; set; } = null!;
+
+        // Request sender info
+        public long FromUserId { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public float ProfitLoss { get; set; }
+    }
 }
