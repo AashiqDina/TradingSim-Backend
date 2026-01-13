0a1d661fb0d2a3ec41b083418a6728597eb741b9
Aashiq Dina <98742552+AashiqDina@users.noreply.github.com>
2026-01-13 17:09:00 +0000

    Create UserReceivedRequest.cs

---
+++ Models/UserReceivedRequest.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TradingSimulator_Backend.Models
+{
+    public class UserReceivedRequest
+    {
+        [Column("UserId")]
+        public long UserId { get; set; }
+
+        [Column("FriendsUserId")]
+        public long FriendsUserId { get; set; }
+
+        [Column("Username")]
+        public string Username { get; set; } = string.Empty;
+
+        [Column("ProfitLoss")]
+        public float ProfitLoss { get; set; }
+    }
+}
