21bf70ba89af4b7a7ee5a2059abebaaaf8688036
Aashiq Dina <98742552+AashiqDina@users.noreply.github.com>
2026-01-13 02:49:44 +0000

    Update StockHistory.cs

--- Models/StockHistory.cs
+++ Models/StockHistory.cs
@@ -1,30 +1,26 @@
 using System;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
-
 using TradingSimulator_Backend.Models;
 
 public class StockHistory
-    {
-        [Column("id")]
-        public long Id { get; set; }
+{
+    [Column("id")]
+    public long Id { get; set; }
 
-        [Column("stockId")]
-        public long StockId { get; set; }
+    [Column("stockid")]
+    public long StockId { get; set; }
 
-        [ForeignKey("StockId")]
-        [System.Text.Json.Serialization.JsonIgnore]
-        public Stock Stock { get; set; } = null!;
+    [ForeignKey("StockId")]
+    [JsonIgnore]
+    public Stock Stock { get; set; } = null!;
 
-        [Column("timestamp")]
-        public DateTime Timestamp { get; set; }
+    [Column("timestamp")]
+    public DateTime Timestamp { get; set; }
 
-        [Column("price")]
-        public decimal Price { get; set; }
+    [Column("price")]
+    public decimal Price { get; set; }
 
-        [Column("quantity")]
-        public decimal Quantity { get; set; }
-    }
-
-
-
+    [Column("quantity")]
+    public decimal Quantity { get; set; }
+}
