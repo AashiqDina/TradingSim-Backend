c212a0f202d8e34a307a1c031f82ae592282d1aa
Aashiq Dina <98742552+AashiqDina@users.noreply.github.com>
2026-01-13 01:47:03 +0000

    Update Portfolio.cs

--- Models/Portfolio.cs
+++ Models/Portfolio.cs
@@ -1,26 +1,30 @@
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Collections.Generic;
 using System.Linq;
+using TradingSimulator_Backend.Models;
 
-public class Portfolio{
-    [Column("id")]
-    public long Id { get; set; }
+namespace TradingSimulator_Backend.Models
+{
+    public class Portfolio
+    {
+        [Column("id")]
+        public long Id { get; set; }
 
-    [Column("user_id")]
-    public long UserId { get; set; }
+        [Column("user_id")]
+        public long UserId { get; set; }
 
-    [ForeignKey("UserId")]
-    public User User { get; set; } = null!;
+        [ForeignKey("UserId")]
+        public User? User { get; set; }
 
-    public List<Stock> Stocks { get; set; } = new();
+        public List<Stock> Stocks { get; set; } = new List<Stock>();
 
-    [NotMapped]
-    public decimal TotalInvested => Stocks.Sum(stock => stock.PurchasePrice * stock.Quantity);
+        [NotMapped]
+        public decimal TotalInvested => Stocks.Sum(stock => stock.PurchasePrice * stock.Quantity);
 
-    [NotMapped]
-    public decimal CurrentValue => Stocks.Sum(stock => stock.TotalValue);
+        [NotMapped]
+        public decimal CurrentValue => Stocks.Sum(stock => stock.TotalValue);
 
-    [NotMapped]
-    public decimal ProfitLoss => CurrentValue - TotalInvested;
+        [NotMapped]
+        public decimal ProfitLoss => CurrentValue - TotalInvested;
+    }
 }
-
