aa98028048bdf4c9a5ce53d87e47a52b20eeb948
Aashiq Dina <98742552+AashiqDina@users.noreply.github.com>
2026-01-13 01:03:48 +0000

    Update Friends.cs

--- Models/Friends.cs
+++ Models/Friends.cs
@@ -1,9 +1,1 @@
-public class Friends
-{
-    public long UserId { get; set; }
-    public User User { get; set; }
 
-    public List<UserObj> FriendsList { get; set; }
-    public List<UserObj> SentRequests { get; set; }
-    public List<UserObj> ReceivedRequests { get; set; }
-}
